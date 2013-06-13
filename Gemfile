92a3b44a7a1be6b70247f5bfd6eae74f1bd8e9c8
sedicted <sedicted@gmail.com>
2013-06-13 16:55:57 -0400

    Working with S3

--- Gemfile
+++ Gemfile
@@ -11,14 +11,13 @@ group :assets do
   gem 'coffee-rails', '~> 3.2.1'
 
   gem 'uglifier', '>= 1.0.3'
-
-  gem "fog"
 end
 
 gem 'jquery-rails'
-gem "carrierwave"
 
+gem 'carrierwave'
 gem 'rmagick', '2.13.2'
+gem 'fog', '~> 1.10.1'
 
 group :production do 
   gem 'pg', '0.12.2'
--- app/uploaders/upjerk_uploader.rb
+++ app/uploaders/upjerk_uploader.rb
@@ -2,15 +2,13 @@ class UpjerkUploader < CarrierWave::Uploader::Base
 
   include CarrierWave::RMagick
 
-  # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
+  # storage :file
+  storage :fog
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}"
   end
 
-  # Create different versions of your uploaded files:
   version :thumb do
     process :resize_to_fill => [180, 180]
   end
