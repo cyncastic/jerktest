0052ee6b7650979e172f06748e14193bf3ddb23b
sedicted <sedicted@gmail.com>
2013-06-13 03:30:29 -0400

    working thumbs

--- app/models/artwork.rb
+++ app/models/artwork.rb
@@ -2,7 +2,6 @@ class Artwork < ActiveRecord::Base
   attr_accessible :img_url, :length, :medium, :thumb_url, :title, :width, :year, :category_id
 
   mount_uploader :img_url, UpjerkUploader
-  mount_uploader :thumb_url, UpjerkUploader
 
   belongs_to :category
 end
