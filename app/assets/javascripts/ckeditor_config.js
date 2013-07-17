0dfc37935b118cf9e66a1ba68899104414b11995
sedicted <sedicted@gmail.com>
2013-07-16 21:53:24 -0400

    updated cke dialog

--- app/assets/javascripts/ckeditor_config.js
+++ app/assets/javascripts/ckeditor_config.js
@@ -3,12 +3,20 @@ CKEDITOR.editorConfig = function( config )
 	config.toolbar = 'MyToolbar';
  
 	config.toolbar_MyToolbar =
-	[
-		{ name: 'styles', items : [ 'Format' ] },
-		{ name: 'insert', items : [ 'Image','Table','HorizontalRule','SpecialChar','PageBreak'] },
-		{ name: 'basicstyles', items : [ 'Bold','Italic','Strike','-','RemoveFormat' ] },
-		{ name: 'paragraph', items : [ 'NumberedList','BulletedList','-','Outdent','Indent','-','Blockquote' ] },
-		{ name: 'links', items : [ 'Link','Unlink','Anchor' ] },
-		{ name: 'tools', items : [ 'Maximize' ] }
+	[	
+		{ name: 'basicstyles', items : [ 'Bold','Italic','Underline','Strike','Subscript','Superscript','-','RemoveFormat' ] },
+		{ name: 'paragraph', items : [ 'NumberedList','BulletedList','-','Outdent','Indent','-','Blockquote','CreateDiv',
+		'-','JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock','-','BidiLtr','BidiRtl' ] },
+		{ name: 'links', items : [ 'Link','Unlink' ] },
+		{ name: 'insert', items : [ 'Image','Table','HorizontalRule' ] },
+		'/',
+		{ name: 'styles', items : [ 'Styles','Format','Font','FontSize' ] },
+		{ name: 'colors', items : [ 'TextColor','BGColor' ] },
+		{ name: 'tools', items : [ 'Maximize', 'ShowBlocks' ] }
 	];
+
+	config.dialog_backgroundCoverColor = 'rgb(0, 0, 0)';
+
+	config.colorButton_colors = '1eb1d7,ed1d7a,b9d548,fdf235';
+
 };
