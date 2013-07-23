b93d961b3aea6baca8640bdd740cf0b5b343b886
sedicted <sedicted@gmail.com>
2013-07-23 01:03:39 -0400

    still screwing with heroku migrations

--- db/migrate/20130723042841_clean_up_my_databse.rb
+++ db/migrate/20130723042841_clean_up_my_databse.rb
@@ -2,7 +2,5 @@ class CleanUpMyDatabse < ActiveRecord::Migration
   def change
     remove_column :blogs, :image
     remove_column :blogs, :position
-
-    drop_table :jerkblogs
   end
 end
--- db/migrate/20130723044831_screwy_one_for_heroku_sync.rb
+++ db/migrate/20130723044831_screwy_one_for_heroku_sync.rb
@@ -1,6 +1,4 @@
 class ScrewyOneForHerokuSync < ActiveRecord::Migration
   def change
-    remove_column :blogs, :image
-    remove_column :blogs, :position
   end
 end
