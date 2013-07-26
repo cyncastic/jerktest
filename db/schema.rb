3dff04dc1d31507272ace4b149b16eb1f1323c74
sedicted <sedicted@gmail.com>
2013-07-26 19:46:27 -0400

    new artwork at top of table and confirmation message added.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130724224537) do
+ActiveRecord::Schema.define(:version => 20130726231906) do
 
   create_table "artworks", :force => true do |t|
     t.string   "title"
@@ -20,10 +20,10 @@ ActiveRecord::Schema.define(:version => 20130724224537) do
     t.integer  "width"
     t.integer  "year"
     t.string   "img_url"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+    t.datetime "created_at",                 :null => false
+    t.datetime "updated_at",                 :null => false
     t.integer  "category_id"
-    t.integer  "position"
+    t.integer  "position",    :default => 0
   end
 
   create_table "blog_images", :force => true do |t|
