add48478af2881fe31b2fee617557bea7628345b
sedicted <sedicted@gmail.com>
2013-08-08 21:29:32 -0400

    gallery commit

--- app/assets/javascripts/lightbox.js
+++ app/assets/javascripts/lightbox.js
@@ -1,30 +1,34 @@
 $(document).ready(function($) {
-
   $('.jerkbox').click(function(e) {
     e.preventDefault();
-    var image_href = $(this).attr("href");
+    theimage = $(this);
+    var image_href = $(this).attr('href');
+    var title = $(this).data('title');
+    var medium = $(this).data('medium');
+    var dimensions = $(this).data('width') + " x " + $(this).data('length');
+    var year = $(this).data('year');
 
     if ($('#lightbox').length > 0) { // #lightbox exists
-
       $('#content').html('<img src="' + image_href + '" />');
       $('#lightbox').show();
-
     }else { //#lightbox does not exist
-
       var lightbox =
         '<div id="lightbox">' +
-            '<div id="content">' +
-                '<img src="' + image_href +'" />' +
+          '<div id="content">' +
+            '<h1>' + title + '</h1>' +
+            '<div>' +
+              '<span>' + medium + '</span>' +
+              '<span>' + dimensions + '</span>' +
+              '<span>' + year + '</span>' +
             '</div>' +
+            '<img src="' + image_href + '" />' +
+          '</div>' +
         '</div>';
-
       $('body').append(lightbox);
     }
   });
-
 });
 
 $(document).on('click', '#lightbox', function(){
   $('#lightbox').hide();
 });
-
